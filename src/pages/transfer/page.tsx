c6785f77d04f252e474fcf2ffd72c202e1f5e73b
Your Name <you@example.com>
2024-06-14 20:40:01 +0300

    build fix

--- src/pages/transfer/page.tsx
+++ src/pages/transfer/page.tsx
@@ -42,6 +42,7 @@ export function Page() {
                 setSelectedDeposit(selected);
                 goToTransfer();
               }}
+              setCurrentCoin={() => {}}
             />
             <TransferBox />
           </Flex>
--- src/pages/withdraw/page.tsx
+++ src/pages/withdraw/page.tsx
@@ -42,6 +42,7 @@ export function Page() {
                 setSelectedDeposit(selected);
                 goToWithdraw();
               }}
+              setCurrentCoin={() => {}}
             />
             <WithdrawBox />
           </Flex>
