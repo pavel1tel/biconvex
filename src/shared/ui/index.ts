c37990f1430b54d452dd55bf3248c272233ae042
WILSONPLUR <yourshows12@gmail.com>
2024-07-17 09:23:27 +0300

    changed import

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -7,4 +7,4 @@ export * from "./sidebar";
 export * from "./container";
 export * from "./show-rows-count";
 export { PageLoader } from "./page-loader";
-export { LoadingScreen } from "./Loading/index";
+export * from "./loading";
